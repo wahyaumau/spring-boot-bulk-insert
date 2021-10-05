4cff262e36314692cbd12bdadac39df469f2c289
Wahyu Maulana <wahyaumau@gmail.com>
2021-10-05 22:10:51 +0700

    init project

---
+++ src/test/java/com/wahyaumau/batchinsert/BatchInsertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wahyaumau.batchinsert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchInsertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
